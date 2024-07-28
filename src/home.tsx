5c870adac9d92f92525d872bbc920897216d2961
bkcheung <b.cheung@hotmail.com>
2024-07-28 16:37:42 -0400

    refactor: update home and main modules to accomodate for App module

--- src/home.tsx
+++ src/home.tsx
@@ -1,11 +1,14 @@
-// import React from 'react'
+interface homeProps{
+    toggleMenu:(e:React.MouseEvent<HTMLButtonElement,MouseEvent>)=>void;
+}
 
-function Home(){
+function Home({toggleMenu}:homeProps){
     return(
-    <div className="bg-home-page bg-cover bg-top w-lvw h-lvh flex-col">
-        <button className="text-2xl m-5 opacity-75">Menu</button>
-        <div className="text-4xl mx-20 my-5 text-center text-black">We're so glad you're here!</div>
-    </div>
+        <div className="bg-home-page bg-cover bg-top w-lvw h-lvh flex-col">
+            <button className="text-2xl m-5 opacity-75"
+                    onClick={toggleMenu}>Menu</button>
+            <div className="text-4xl mx-20 my-5 text-center text-black">We're so glad you're here!</div>
+        </div>
     )
 }
 
